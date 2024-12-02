232369920097e60abffd747909c411a6fa6a1183
whatcodehaveyougit <sigurdwatt@gmail.com>
2024-12-02 20:16:07 +0000

    Create skeleton for RK website, phase 1

--- templates/shortcodes/custom-acf-block.php
+++ templates/shortcodes/custom-acf-block.php
@@ -1,14 +1,25 @@
-<div class="featured-section">
-  <div class="featured-image">
+<?php
+$protocol = (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off') ? 'https://' : 'http://';
+$host = $_SERVER['HTTP_HOST'];
+?>
+
+<div class="hero-component">
+  <div class="hero-image">
     <img src="http://localhost:10033/wp-content/uploads/2024/11/Screenshot-2024-11-29-at-14.12.06.png"
       alt="A cup of coffee with latte art" />
     <div>
-      <span>Monday to Friday <?php echo esc_html($monday_to_friday_hours); ?></span>
-      <span>Saturday and Sunday<?php echo esc_html($sat_and_sun_hours); ?></span>
+      <span><b>Monday to Friday</b><span><?php echo esc_html($monday_to_friday_hours); ?></span></span>
+      <span><b>Saturday and Sunday</b><span><?php echo esc_html($sat_and_sun_hours); ?></span></span>
     </div>
   </div>
-  <div class="featured-content">
-    <h5>Breakfast | Brunch | Drinks | Cakes</h5>
+  <div class="hero-content">
+    <h5>
+      <a href="<?php echo $protocol . $host; ?>/brunch-in-abbeyhill">Breakfast</a> |
+      <a href="<?php echo $protocol . $host; ?>/brunch-in-abbeyhill">Brunch</a> |
+      <a href="<?php echo $protocol . $host; ?>/drinks-in-abbeyhill">Drinks</a> |
+      <a href="<?php echo $protocol . $host; ?>/cakes-in-abbeyhill">Cakes</a>
+    </h5>
+    <div aria-hidden="true" class="wp-block-spacer"></div>
     <h1><?php echo esc_html($featured_section_1_title); ?></h1>
     <p>
       <?php echo esc_html($featured_section_1_text); ?>
